2bfe42457ce21f135890e4f73025a0391a0341de
free1990 <workmac@WorkMacdeiMac.local>
2015-03-17 17:46:07 +0800

    QR code scan view

---
+++ ScanQRCode/ScanQRCode/ScanView.h
@@ -0,0 +1,13 @@
+//
+//  ScanView.h
+//  ScanQRCode
+//
+//  Created by John on 15/3/17.
+//  Copyright (c) 2015年 julong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScanView : NSObject
+
+@end
---
+++ ScanQRCode/ScanQRCode/ScanView.m
@@ -0,0 +1,13 @@
+//
+//  ScanView.m
+//  ScanQRCode
+//
+//  Created by John on 15/3/17.
+//  Copyright (c) 2015年 julong. All rights reserved.
+//
+
+#import "ScanView.h"
+
+@implementation ScanView
+
+@end
